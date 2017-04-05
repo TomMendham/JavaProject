0368a1a9040092f24bcfd0d4f46ffab3b218aa41
33ej33 <ejulenyi@gmail.com>
2017-04-05 15:16:40 +0100

    Register added to client

--- src/javaproject/SocketClient.java
+++ src/javaproject/SocketClient.java
@@ -87,5 +87,57 @@ public class SocketClient {
       System.out.println("Exception: " + g);
     }   
  }
+ 
+  public String registering(String username, String password, String dateOfBirth, String[]genres){
+     String isCorrect;
+     String host = "localhost";
+     int port = 19999;
+     try {
+         //Find the INET 
+         InetAddress address = InetAddress.getByName(host);
+         Socket connection = new Socket(address, port);
+         
+         //Write the output to a buffered output and then to the writer
+         BufferedOutputStream bos = new BufferedOutputStream(connection.getOutputStream());
+         OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
+         
+         String genresString = "";
+         //Adding the genres into one string
+         for (int i = 0; i < genres.length; i++)
+         {
+             genresString += genres[i];
+             genresString += "-";
+         }
+         
+         //Message to send over server
+         String message = username + "-" + password + "-" + dateOfBirth + "-" + genresString + "-" + "registering" + (char) 13;
+         //Writing the message and flushing the server
+         osw.write(message);
+         osw.flush();
+         
+         BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
+         InputStreamReader isr = new InputStreamReader(bis, "US-ASCII");
+         StringBuffer input = new StringBuffer();
+         
+          int c;
+          while ( (c = isr.read()) != 13){
+              input.append( (char) c);
+          }
+                       
+         //Close the connection
+         connection.close();
+         isCorrect = input.toString();
+     }
+     
+    catch (IOException f) {
+      System.out.println("IOException: " + f);
+      isCorrect = "server";
+    }
+    catch (Exception g) {
+      System.out.println("Exception: " + g);
+      isCorrect = "server";
+    }   
+    return (isCorrect);
+ }
 }
 
